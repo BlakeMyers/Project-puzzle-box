b183781f8ee025de50ffbc69a6399aaf27ec7eca
jbarger25 <32649192+jbarger25@users.noreply.github.com>
2019-12-02 13:57:02 -0800

    Prototypes for object manipulation scripts

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    [SerializeField]
+    private string moveDirection = "x";
+    private Vector3 point;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        if (moveDirection.Equals("x"))
+        {
+            point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                        (transform.position.y - Camera.main.transform.position.y),
+                        (transform.position.z - Camera.main.transform.position.z)));
+
+            point.y = transform.position.y;
+            point.z = transform.position.z;
+            transform.position = point;
+        }
+        else if (moveDirection.Equals("y"))
+        {
+            point = Camera.main.ScreenToWorldPoint(new Vector3((transform.position.x - Camera.main.transform.position.x),
+                        (Input.mousePosition.y),
+                        (transform.position.z - Camera.main.transform.position.z)));
+
+            point.x = transform.position.x;
+            point.z = transform.position.z;
+            transform.position = point;
+        }
+        else if (moveDirection.Equals("z"))
+        {
+            point = Camera.main.ScreenToWorldPoint(new Vector3((transform.position.x - Camera.main.transform.position.x),
+                        (transform.position.y - Camera.main.transform.position.y),
+                        (Input.mousePosition.z)));
+
+            point.x = transform.position.x;
+            point.y = transform.position.y;
+            transform.position = point;
+        }
+    }
+
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDrag()
+    {
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
+    }
+}
