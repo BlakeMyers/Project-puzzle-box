1802278e250a529bc4c27af1886163ee6ce564d4
Jacob <jacob@goldenneedham.com>
2019-12-03 23:36:47 -0800

    Made the RotateObject a bit better

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -4,31 +4,37 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
-    //Instead of using Transform use Rigidbody.MoveRotation()
-    //That way we get collisions, and can inset objects
-
     [SerializeField]
-    private float speed = 500.0f;
+    private float speed = 5.0f;
     private Vector3 rotateVector;
-    public Rigidbody rigidBody;
+    Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        
+        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(Input.GetMouseButton(0)){
+            rotateVector = new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed;
+            rotateVector.y *= -1;
 
-    void OnMouseDrag()
-    {
-        rotateVector = new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed;
-        rotateVector.y *= -1;
-        //transform.Rotate(rotateVector);
-        Quaternion deltaRotation = Quaternion.Euler(rotateVector);
-        rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+            transform.RotateAround(this.transform.position, rotateVector, 1 * speed);
+
+            //this.transform.rotation = (this.transform.rotation * deltaRotation);
+        }
+
+        if(Input.GetMouseButton(1)){
+            rotateVector = new Vector3(0, 0, Input.GetAxis("Mouse Y") - Input.GetAxis("Mouse X")) * Time.deltaTime * speed;
+            rotateVector.y *= -1;
+
+            transform.RotateAround(this.transform.position, rotateVector, 1 * speed);
+
+            //this.transform.rotation = (this.transform.rotation * deltaRotation);
+        }
     }
 }
