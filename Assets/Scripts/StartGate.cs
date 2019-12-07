97bea7b3b96f26a53457859244c1711c67516d8a
jbarger25 <32649192+jbarger25@users.noreply.github.com>
2019-12-07 13:42:17 -0800

    Added animations to start gates

--- Assets/Scripts/StartGate.cs
+++ Assets/Scripts/StartGate.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class StartGate : MonoBehaviour
 {
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,6 +18,7 @@ public class StartGate : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        transform.position = new Vector3(0, 0, 0);
+        //transform.position = new Vector3(0, 0, 0);
+        this.animator.SetBool("DoAnimate", true);
     }
 }
