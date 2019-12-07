16086bfc0e191932edd1cd5688befb4c3bef2d35
jbarger25 <32649192+jbarger25@users.noreply.github.com>
2019-12-07 15:07:02 -0800

    Added material changes to puzzles to indicate which ones are solved

--- Assets/Scripts/RollingObject.cs
+++ Assets/Scripts/RollingObject.cs
@@ -27,5 +27,55 @@ public class RollingObject : MonoBehaviour
             rigidBody.isKinematic = true;
             UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
         }
+        if (col.gameObject.tag == "Goal1")
+        {
+            Debug.Log("Cube1");
+            GameObject puzzle = GameObject.Find("Cube1");
+            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
+            meshRenderer.material = meshRenderer.materials[1];
+            transform.position = new Vector3(0, 0, 0);
+            rigidBody.isKinematic = true;
+            UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
+        }
+        if (col.gameObject.tag == "Goal2")
+        {
+            Debug.Log("Cube2");
+            GameObject puzzle = GameObject.Find("puzzle2");
+            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
+            meshRenderer.material = meshRenderer.materials[1];
+            transform.position = new Vector3(0, 0, 0);
+            rigidBody.isKinematic = true;
+            UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
+        }
+        if (col.gameObject.tag == "Goal3")
+        {
+            Debug.Log("Cube2");
+            GameObject puzzle = GameObject.Find("Cube3");
+            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
+            meshRenderer.material = meshRenderer.materials[1];
+            transform.position = new Vector3(0, 0, 0);
+            rigidBody.isKinematic = true;
+            UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
+        }
+        if (col.gameObject.tag == "Goal4")
+        {
+            Debug.Log("Cube4");
+            GameObject puzzle = GameObject.Find("Cube4");
+            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
+            meshRenderer.material = meshRenderer.materials[1];
+            transform.position = new Vector3(0, 0, 0);
+            rigidBody.isKinematic = true;
+            UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
+        }
+        if (col.gameObject.tag == "Goal5")
+        {
+            Debug.Log("Cube5");
+            GameObject puzzle = GameObject.Find("Cube5");
+            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
+            meshRenderer.material = meshRenderer.materials[1];
+            transform.position = new Vector3(0, 0, 0);
+            rigidBody.isKinematic = true;
+            UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
+        }
     }
 }
