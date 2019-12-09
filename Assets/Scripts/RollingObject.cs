db1d722bede298e5ffea0f61b6862ad0d0153034
jbarger25 <32649192+jbarger25@users.noreply.github.com>
2019-12-09 11:18:16 -0800

    Changed script and object names to make sure correct material is set on level start

--- Assets/Scripts/RollingObject.cs
+++ Assets/Scripts/RollingObject.cs
@@ -6,11 +6,26 @@ public class RollingObject : MonoBehaviour
 {
     public Rigidbody rigidBody;
     GameObject UI_Manager;
+    MeshRenderer meshRenderer1;
+    MeshRenderer meshRenderer2;
+    MeshRenderer meshRenderer3;
+    MeshRenderer meshRenderer4;
+    MeshRenderer meshRenderer5;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         UI_Manager = GameObject.Find("UI_Manager");
+        meshRenderer1 = GameObject.Find("Cube1").GetComponent<MeshRenderer>();
+        meshRenderer1.material = meshRenderer1.materials[0];
+        meshRenderer2 = GameObject.Find("Cube2").GetComponent<MeshRenderer>();
+        meshRenderer2.material = meshRenderer2.materials[0];
+        meshRenderer3 = GameObject.Find("Cube3").GetComponent<MeshRenderer>();
+        meshRenderer3.material = meshRenderer3.materials[0];
+        meshRenderer4 = GameObject.Find("Cube4").GetComponent<MeshRenderer>();
+        meshRenderer4.material = meshRenderer4.materials[0];
+        meshRenderer5 = GameObject.Find("Cube5").GetComponent<MeshRenderer>();
+        meshRenderer5.material = meshRenderer5.materials[0];
     }
 
     // Update is called once per frame
@@ -30,9 +45,7 @@ public class RollingObject : MonoBehaviour
         if (col.gameObject.tag == "Goal1")
         {
             Debug.Log("Cube1");
-            GameObject puzzle = GameObject.Find("Cube1");
-            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
-            meshRenderer.material = meshRenderer.materials[1];
+            meshRenderer1.material = meshRenderer1.materials[1];
             transform.position = new Vector3(0, 0, 0);
             rigidBody.isKinematic = true;
             UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
@@ -40,19 +53,15 @@ public class RollingObject : MonoBehaviour
         if (col.gameObject.tag == "Goal2")
         {
             Debug.Log("Cube2");
-            GameObject puzzle = GameObject.Find("puzzle2");
-            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
-            meshRenderer.material = meshRenderer.materials[1];
+            meshRenderer2.material = meshRenderer2.materials[1];
             transform.position = new Vector3(0, 0, 0);
             rigidBody.isKinematic = true;
             UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
         }
         if (col.gameObject.tag == "Goal3")
         {
-            Debug.Log("Cube2");
-            GameObject puzzle = GameObject.Find("Cube3");
-            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
-            meshRenderer.material = meshRenderer.materials[1];
+            Debug.Log("Cube3");
+            meshRenderer3.material = meshRenderer3.materials[1];
             transform.position = new Vector3(0, 0, 0);
             rigidBody.isKinematic = true;
             UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
@@ -60,9 +69,7 @@ public class RollingObject : MonoBehaviour
         if (col.gameObject.tag == "Goal4")
         {
             Debug.Log("Cube4");
-            GameObject puzzle = GameObject.Find("Cube4");
-            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
-            meshRenderer.material = meshRenderer.materials[1];
+            meshRenderer4.material = meshRenderer4.materials[1];
             transform.position = new Vector3(0, 0, 0);
             rigidBody.isKinematic = true;
             UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
@@ -70,9 +77,7 @@ public class RollingObject : MonoBehaviour
         if (col.gameObject.tag == "Goal5")
         {
             Debug.Log("Cube5");
-            GameObject puzzle = GameObject.Find("Cube5");
-            MeshRenderer meshRenderer = puzzle.GetComponent<MeshRenderer>();
-            meshRenderer.material = meshRenderer.materials[1];
+            meshRenderer5.material = meshRenderer5.materials[1];
             transform.position = new Vector3(0, 0, 0);
             rigidBody.isKinematic = true;
             UI_Manager.GetComponent<UI_Controller>().puzzles_solved += 0.5f;
